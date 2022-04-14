42bd92bae76395bb7fe4ac9f7f0e8595e4527f52
rhc07 <rorie@ori.co>
2022-04-14 16:17:14 +0100

    added basic functionality

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
-func main() {
+import (
+	"github.com/labstack/echo"
+	"net/http"
+)
 
+type HelloWorld struct {
+	Message string `json:"message"`
 }
 
+func main() {
+	e := echo.New()
+	e.GET("/", Greeting)
+	e.Logger.Fatal(e.Start(":3000"))
+}
+
+func Greeting(ctx echo.Context) error {
+	return ctx.JSON(http.StatusOK, HelloWorld{
+		Message: "Hello World",
+	})
+}
