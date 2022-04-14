e81f18727d70145f64203a14a2f9ade6b6d4f4cd
rhc07 <rorie@ori.co>
2022-04-14 16:32:27 +0100

    added params routing function

--- main.go
+++ main.go
@@ -12,11 +12,19 @@ type HelloWorld struct {
 func main() {
 	e := echo.New()
 	e.GET("/", Greeting)
+	e.GET("/hello/:name", GreetingWithParams)
 	e.Logger.Fatal(e.Start(":3000"))
 }
 
 func Greeting(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, HelloWorld{
 		Message: "Hello World",
+	})
+}
+
+func GreetingWithParams(ctx echo.Context) error {
+	params := ctx.Param("name")
+	return ctx.JSON(http.StatusOK, HelloWorld{
+		Message: "Hello World, my name is " + params,
 	})
 }
