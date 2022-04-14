ced7fb2726aad380c18c93bc73111b74263984ab
rhc07 <rorie@ori.co>
2022-04-14 16:37:02 +0100

    added query params function

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 	e := echo.New()
 	e.GET("/", Greeting)
 	e.GET("/hello/:name", GreetingWithParams)
+	e.GET("/hello-queries", GreetingWithQuery)
 	e.Logger.Fatal(e.Start(":3000"))
 }
 
@@ -26,5 +27,12 @@ func GreetingWithParams(ctx echo.Context) error {
 	params := ctx.Param("name")
 	return ctx.JSON(http.StatusOK, HelloWorld{
 		Message: "Hello World, my name is " + params,
+	})
+}
+
+func GreetingWithQuery(ctx echo.Context) error {
+	query := ctx.QueryParam("name")
+	return ctx.JSON(http.StatusOK, HelloWorld{
+		Message: "Hello World, my name is " + query + " and I'm using queries",
 	})
 }
